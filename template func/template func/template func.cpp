fa876537ddb5c8e028be7eaaccf00908a935018e
DICHDN <146694203+DICHND-CPP@users.noreply.github.com>
2024-03-04 07:12:16 +0300

    first commit

---
+++ template func/template func/template func.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <vector>
+
+
+template  <class Temp>
+void pow_2(Temp a)
+{
+    std::cout << a * a << std::endl;
+}
+template  <>
+void pow_2(std::vector<int> a)
+{
+    for (int i = 0; i < a.size(); ++i)
+    {
+        std::cout << a[i] * a[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+
+
+int main()
+{
+    std::vector <int> arr = { 1,2,3,4,5,6,7 };
+    int b = 10;
+    double c = 9;
+
+    pow_2(arr);
+    pow_2(b);
+    pow_2(c);
+
+}
+
